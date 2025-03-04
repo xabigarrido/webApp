07aae8b41aed0623191291652b6043dbae84c9cd
xabigarrido <xabigarrido@gmail.com>
2025-03-04 20:47:11 +0100

    first comwsss2w2dddt

--- src/pages/EntrarEmpresa.jsx
+++ src/pages/EntrarEmpresa.jsx
@@ -1,32 +1,76 @@
-import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Circle } from "react-leaflet";
+import { useEffect, useRef, useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Circle,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-
-export default function Mapa() {
-  const [userPosition, setUserPosition] = useState(null); // Ubicación del usuario
-  const [expoPosition, setExpoPosition] = useState(null); // Ubicación desde Expo
-  const defaultPosition = [40.4168, -3.7038]; // Coordenadas por defecto (Madrid)
+import L from "leaflet";
+function AutoPopupMarker({ position }) {
+  const markerRef = useRef(null);
 
   useEffect(() => {
-    // Obtener la ubicación del usuario
+    // Esperamos 500ms para asegurarnos de que el marcador se haya renderizado
+    const timer = setTimeout(() => {
+      if (markerRef.current) {
+        markerRef.current.openPopup();
+      }
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [markerRef]);
+
+  return (
+    <Marker position={position} ref={markerRef} icon={customIcon}>
+      <Popup>¡Estás aquí!</Popup>
+    </Marker>
+  );
+}
+const customIcon = new L.Icon({
+  iconUrl: "/images/marker.png", // Ruta relativa a la carpeta 'public'
+  iconSize: [50, 50], // Ajusta según el tamaño de tu imagen
+  iconAnchor: [20, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+function ClickHandler({ onMapClick }) {
+  useMapEvents({
+    click(e) {
+      onMapClick(e); // Asegúrate de que el evento sea correctamente manejado
+    },
+  });
+  return null;
+}
+
+export default function CrearEmpresa() {
+  const [position, setPosition] = useState([37.7749, -122.4194]); // San Francisco inicial
+  const [loading, setLoading] = useState(true);
+  const [metrosRange, setMetrosRange] = useState(500);
+  const [marker, setMarker] = useState([37.7749, -122.4194]); // San Francisco inicial
+
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (pos) => {
-        setUserPosition([pos.coords.latitude, pos.coords.longitude]);
+        const userLocation = [pos.coords.latitude, pos.coords.longitude];
+        setPosition(userLocation);
+        setMarker(userLocation); // Aseguramos que el marcador también tenga la ubicación inicial
+        setLoading(false);
       },
-      (error) => {
-        console.error("Error obteniendo ubicación del usuario:", error);
-        setUserPosition(defaultPosition); // Si falla, usa la ubicación por defecto
-      },
-      { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+      () => {
+        alert("No se pudo obtener la ubicación");
+        setLoading(false);
+      }
     );
 
-    // Escuchar mensajes desde Expo
     const handleMessage = (event) => {
       try {
         if (typeof event.data === "string") {
           const coords = JSON.parse(event.data);
           if (coords.lat && coords.lng) {
-            setExpoPosition([coords.lat, coords.lng]);
+            setMarker([coords.lat, coords.lng]);
           }
         }
       } catch (error) {
@@ -38,34 +82,156 @@ export default function Mapa() {
     return () => window.removeEventListener("message", handleMessage);
   }, []);
 
+  const handleMapClick = (event) => {
+    const { lat, lng } = event.latlng;
+    setMarker([lat, lng]); // Guarda solo un marcador
+  };
+
+  const handleSendLocation = () => {
+    const miData = { ubicacionEmpresa: marker, metrosRange };
+
+    // Asegúrate de que window.ReactNativeWebView esté definido antes de intentar usarlo
+    if (typeof window !== "undefined" && window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(JSON.stringify(miData));
+    } else {
+      console.error("ReactNativeWebView no está disponible.");
+    }
+  };
+
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <MapContainer
-        center={userPosition || defaultPosition} // Se carga el mapa incluso si la posición del usuario es null
-        zoom={15}
-        style={{ height: "100%", width: "100%" }}
-      >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-        {/* Marcador del usuario */}
-        {userPosition && <Marker position={userPosition} />}
-
-        {/* Marcador y círculo de la ubicación desde Expo */}
-        {expoPosition && (
-          <>
-            <Marker position={expoPosition} />
-            <Circle
-              center={expoPosition}
-              radius={500}
-              pathOptions={{
-                color: "red",
-                fillColor: "red",
-                fillOpacity: 0.2,
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {loading ? (
+        <p>Cargando ubicación...</p>
+      ) : (
+        <>
+          {/* Contenedor superior con input y botón */}
+          {/* <div
+            style={{
+              width: "100%",
+              backgroundColor: "#ffffff",
+              padding: "20px 30px",
+              borderRadius: "15px",
+              textAlign: "center",
+              position: "fixed",
+              top: "20px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              zIndex: 9999,
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+              maxWidth: "500px",
+              transition: "all 0.3s ease",
+            }}
+          >
+            <p
+              style={{
+                fontSize: "24px",
+                fontWeight: "600",
+                color: "#333",
+                marginBottom: "10px",
               }}
-            />
-          </>
-        )}
-      </MapContainer>
+            >
+              Marca el lugar de tu empresa
+            </p>
+            <p
+              style={{
+                fontSize: "16px",
+                color: "#555",
+                marginBottom: "20px",
+                lineHeight: "1.5",
+              }}
+            >
+              Metros de distancia para poder acceder
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "15px",
+                flexDirection: "column",
+              }}
+            >
+              <div style={{ width: "100%" }}>
+                <input
+                  type="number"
+                  inputMode="numeric"
+                  placeholder="500 metros"
+                  onChange={(event) =>
+                    setMetrosRange(Number(event.target.value))
+                  }
+                  style={{
+                    width: "100%",
+                    padding: "12px 18px",
+                    fontSize: "16px",
+                    borderRadius: "8px",
+                    border: "2px solid #ddd",
+                    outline: "none",
+                    boxSizing: "border-box",
+                    transition: "all 0.3s ease",
+                    marginBottom: "15px",
+                  }}
+                  onFocus={(e) => (e.target.style.border = "2px solid #007BFF")}
+                  onBlur={(e) => (e.target.style.border = "2px solid #ddd")}
+                />
+                <button
+                  onClick={handleSendLocation}
+                  style={{
+                    backgroundColor: "#007BFF",
+                    color: "#fff",
+                    padding: "12px 30px",
+                    borderRadius: "8px",
+                    border: "none",
+                    fontSize: "16px",
+                    cursor: "pointer",
+                    transition: "all 0.3s ease",
+                    width: "100%",
+                    fontWeight: "600",
+                  }}
+                >
+                  Agregar ubicación
+                </button>
+              </div>
+            </div>
+          </div> */}
+
+          {/* Contenedor del mapa que ocupa todo el espacio restante */}
+          <div style={{ flexGrow: 1 }}>
+            <MapContainer
+              center={position}
+              zoom={15}
+              style={{ height: "100%", width: "100%" }}
+            >
+              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              <AutoPopupMarker position={position} />
+
+              {marker && (
+                <>
+                  <Marker position={marker} icon={customIcon}>
+                    <Popup>Ubicación seleccionada</Popup>
+                  </Marker>
+                  <Circle
+                    center={marker}
+                    radius={metrosRange}
+                    pathOptions={{
+                      color: "blue",
+                      fillColor: "blue",
+                      fillOpacity: 0.2,
+                    }}
+                  />
+                </>
+              )}
+              {/* <ClickHandler onMapClick={handleMapClick} /> */}
+            </MapContainer>
+          </div>
+        </>
+      )}
     </div>
   );
 }
