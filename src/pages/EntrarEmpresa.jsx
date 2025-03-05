661d974aa6a59c8a9705f4a886486cf7356e5384
xabigarrido <xabigarrido@gmail.com>
2025-03-05 07:47:07 +0100

    first comwsss2w2dddss2ssts2s2s

--- src/pages/EntrarEmpresa.jsx
+++ src/pages/EntrarEmpresa.jsx
@@ -37,7 +37,7 @@ export default function CrearEmpresa() {
   const [dataEmpresa, setDataEmpresa] = useState({
     nameEmpresa: "dewdew",
     distancePick: 0,
-    trabajando: "false",
+    trabajando: "",
   });
   useEffect(() => {
     setDataEmpresa({
